782ea17eac667bf742bc626f22acdac25a9e5a95
Uclides Gil <uclidesgil@gmail.com>
2017-05-26 18:18:30 -0400

    add checkout and cartDetail components

---
+++ src/app/store/cartDetail.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: `<div> <h3>Cart Detail Component<h3></div>`
+})
+export class CartDetailComponent { }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
-import {CounterDirective} from './counter.directive';
-import {CartSummaryComponent} from './cartsummary.component';
+import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartsummary.component';
+import { CartDetailComponent } from './cartDetail.component';
+import { CheckoutComponent } from './checkout.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
-    exports: [StoreComponent]
+    declarations: [StoreComponent, CounterDirective, 
+        CartSummaryComponent, CartDetailComponent, CheckoutComponent],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
 })
 export class StoreModule { }
